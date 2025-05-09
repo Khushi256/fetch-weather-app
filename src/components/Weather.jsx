068191c134cb9b0dd0539278914772ef9a6d7875
Khushi256 <khushim1109@gmail.com>
2025-05-09 20:57:02 +0530

    Third commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React ,{ useEffect }  from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import temp_icon from '../assets/sun-cloud.png'
 import humidity_icon from '../assets/humidity.png'
 import wind_icon from '../assets/wind.png'
+import cloud_snow_icon from '../assets/cloud-snow.png'
+import cloud_thunder_icon from '../assets/cloud-thunder.png'
+import cloud_icon from '../assets/cloud.png'
+import moon_icon from '../assets/moon.png'
+import rain_icon from '../assets/rain.png'
+import sun_cloud_icon from '../assets/sun-cloud.png'
+import sun_icon from '../assets/sun.png'
+
 
 const Weather = () => {
+  
+  const search = async (city) => {
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+   useEffect (() => {
+    search("Delhi");
+   },[])
+
+  
   return (
     <div className='weather'>
       <div className='search-bar'>
         <input type="text" placeholder='Search' />
-        <img src={search_icon} id="search-icon"alt=""/>
+        <img src={search_icon} className="search-icon"alt=""/>
       </div>
       <div className='temp-loc'>
-        <img src={temp_icon} className="temp-icon"alt=""/>
+        <img src={temp_icon} className="temp-icon" alt=""/>
         <p className='temprature'>24&deg;c</p>
         <h1 className='location'>City</h1>
       </div>
